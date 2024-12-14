767916dac238c7ace7db79677860f381e5f52b62
Stephane Zermatten <szermatt@gmx.net>
2024-12-14 22:29:34 +0200

    feat: Copy and adapt utilities from termgrab-ert.el.

--- test/turtles-ert-test.el
+++ test/turtles-ert-test.el
@@ -30,3 +30,175 @@
   (turtles-ert-test)
 
   (should (equal 1 2)))
+
+(ert-deftest turtles-ert-test-to-string-noarg ()
+  (turtles-ert-test)
+  (ert-with-test-buffer ()
+    (insert "hello, world")
+    (should (equal "hello, world" (turtles-to-string)))))
+
+(ert-deftest turtles-ert-test-to-string-buf ()
+  (turtles-ert-test)
+
+  (let (test-buffer)
+    (ert-with-test-buffer ()
+      (setq test-buffer (current-buffer))
+      (insert "hello, world")
+      (with-temp-buffer
+        (should (equal "hello, world" (turtles-to-string :buf test-buffer)))))))
+
+(ert-deftest turtles-ert-test-to-string-win ()
+  (turtles-ert-test)
+
+  (let (buf1 buf2)
+    (ert-with-test-buffer (:name "buf1")
+      (setq buf1 (current-buffer))
+      (insert "hello, world")
+
+      (ert-with-test-buffer (:name "buf2")
+        (setq buf2 (current-buffer))
+        (insert "foobar")
+
+        (split-window-below nil (frame-root-window))
+        (pcase-let ((`(,win1 ,win2) (window-list)))
+          (set-window-buffer win1 buf1)
+          (set-window-buffer win2 buf2)
+
+      (should (equal "hello, world" (turtles-to-string :win win1)))
+      (should (equal "foobar" (turtles-to-string :win win2))))))))
+
+(ert-deftest turtles-ert-test-to-string-faces ()
+  (turtles-ert-test)
+  (ert-with-test-buffer ()
+    (insert (propertize "hello" 'face 'error))
+    (insert ", ")
+    (insert (propertize "world" 'face 'success))
+    (should (equal "[hello], {world}"
+                   (turtles-to-string :faces '((error "[]")
+                                                (success "{" "}")))))))
+
+(ert-deftest turtles-ert-test-to-string-region ()
+  (turtles-ert-test)
+
+  (ert-with-test-buffer ()
+    (insert "baa, baa, black sheep, have you any wool?")
+    (goto-char (point-min))
+    (search-forward "baa, black")
+    (push-mark (match-beginning 0) 'nomsg)
+    (search-forward "sheep")
+    (activate-mark)
+
+    (should
+     (equal
+      "baa, [baa, black sheep], have you any wool?"
+      (turtles-to-string :region "[]")))))
+
+(ert-deftest turtles-ert-test-to-string-point ()
+  (turtles-ert-test)
+
+  (ert-with-test-buffer ()
+    (insert "baa, baa, black sheep, have you any wool?")
+    (goto-char (point-min))
+    (search-forward "black")
+
+    (should
+     (equal
+      "baa, baa, black>< sheep, have you any wool?"
+      (turtles-to-string :point "><")))))
+
+(ert-deftest turtles-ert-test-to-string-point-and-region ()
+  (turtles-ert-test)
+
+  (ert-with-test-buffer ()
+    (insert "baa, baa, black sheep, have you any wool?")
+    (goto-char (point-min))
+    (search-forward "baa, black")
+    (push-mark (match-beginning 0) 'nomsg)
+    (search-forward "sheep")
+    (activate-mark)
+
+    (should
+     (equal
+      "baa, [baa, black sheep><], have you any wool?"
+      (turtles-to-string :point "><" :region "[]")))))
+
+(ert-deftest turtles-ert-test-with-grab-buffer-noarg ()
+  (turtles-ert-test)
+
+  (ert-with-test-buffer ()
+    (insert "hello, world")
+    (turtles-with-grab-buffer ()
+      (should (equal "hello, world"
+                     (string-trim (buffer-string)))))))
+
+(ert-deftest turtles-ert-test-with-grab-buffer-result ()
+  (turtles-ert-test)
+
+  (ert-with-test-buffer ()
+    (insert "hello, world")
+    (should (equal "hello, world"
+                   (turtles-with-grab-buffer ()
+                     (string-trim (buffer-string)))))))
+
+(ert-deftest turtles-ert-test-with-grab-buffer-buf ()
+  (turtles-ert-test)
+
+  (let (test-buffer)
+    (ert-with-test-buffer ()
+      (setq test-buffer (current-buffer))
+      (insert "hello, world")
+      (with-temp-buffer
+        (turtles-with-grab-buffer (:buf test-buffer)
+          (should (equal "hello, world"
+                         (string-trim (buffer-string)))))))))
+
+
+(ert-deftest turtles-ert-test-with-grab-buffer-win ()
+  (turtles-ert-test)
+
+  (let (buf1 buf2)
+    (ert-with-test-buffer (:name "buf1")
+      (setq buf1 (current-buffer))
+      (insert "hello, world")
+
+      (ert-with-test-buffer (:name "buf2")
+        (setq buf2 (current-buffer))
+        (insert "foobar")
+
+        (split-window-below nil (frame-root-window))
+        (pcase-let ((`(,win1 ,win2) (window-list)))
+          (set-window-buffer win1 buf1)
+          (set-window-buffer win2 buf2)
+
+          (turtles-with-grab-buffer (:win win1)
+            (should (equal "hello, world"
+                           (string-trim (buffer-string)))))
+          (turtles-with-grab-buffer (:win win2)
+            (should (equal "foobar"
+                           (string-trim (buffer-string))))))))))
+
+(ert-deftest turtles-ert-test-with-grab-buffer-faces ()
+  (turtles-ert-test)
+
+  (ert-with-test-buffer ()
+    (insert (propertize "error" 'face 'error))
+    (insert ", ")
+    (insert (propertize "success" 'face 'success))
+    (turtles-with-grab-buffer (:faces '(success error))
+      (goto-char (point-min))
+      (search-forward "error")
+      (should (equal 'error (get-text-property (1- (point)) 'face)))
+      (search-forward "success")
+      (should (equal 'success (get-text-property (1- (point)) 'face))))))
+
+(ert-deftest turtles-ert-read-from-minibuffer ()
+  (turtles-ert-test)
+
+  (ert-with-buffer-selected ()
+    (should
+     (equal "hello"
+            (turtles-read-from-minibuffer
+                (read-from-minibuffer "Prompt: ")
+              (execute-kbd-macro (kbd "hello"))
+              (should (equal "Prompt: hello" (turtles-to-string)))
+              (execute-kbd-macro (kbd "RET")))))))
--- turtles-ert.el
+++ turtles-ert.el
@@ -24,6 +24,7 @@
 ;;; Code:
 
 (require 'ert)
+(require 'ert-x)
 (require 'cl-lib)
 
 (require 'turtles)
@@ -73,6 +74,182 @@ them into the local `ert-test' instance."
     (funcall func test)
     (when turtles-ert--result
       (setf (ert-test-most-recent-result test) turtles-ert--result))))
+
+(cl-defun turtles-to-string (&key (name "grab")
+                                   frame win buf minibuffer
+                                   faces region point (trim t))
+  "Grab a section of the terminal and return the result as a string.
+
+With no arguments, this function renders the current buffer in
+the turtles frame, and returns the result as a string.
+
+The rendered buffer is grabbed into an ERT test buffer with the
+name \"grab\". When running interactively, ERT keeps such buffers
+when tests fail so they can be checked out. Specify the keyword
+argument NAME to modify the name of the test buffer.
+
+The following keyword arguments modify what is grabbed:
+
+  - The key argument BUF specifies a buffer to capture. It
+    defaults to the current buffer. The buffer is installed into
+    the single window of `turtles-frame', rendered, then
+    grabbed.
+
+  - The key argument WIN specifies a window to grab. The window
+    must be part of `turtles-frame'.
+
+  - Set the key argument MINIBUFFER to t to capture the content
+    of the minibuffer window of `turtles-frame'.
+
+  - Set the key argument FRAME to t to capture the whole frame.
+
+The following keyword arguments post-process what was grabbed:
+
+  - Set the key argument TRIM to nil to not trim the newlines
+    at the end of the grabbed string. Without trimming, there
+    is one newline per line of the grabbed window, even if
+    the buffer content is shorter.
+
+  - Pass a string to the key argument POINT to insert at point,
+    so that position is visible in the returned string.
+
+  - The key argument REGION makes the active region visible in
+    the returned string. Pass a string composed of opening and
+    closing strings of the same length, such as \"[]\" or
+    \"/**/\", to mark the beginning and end of the region.
+    Alternatively, you can also pass a list made up of two
+    strings, the opening and closing string, which then don't
+    need to be of the same size. See also `turtles-mark-region'.
+
+  - The key argument FACES makes a specific set of faces visible
+    in the returned string. Pass an alist with the symbols of the
+    faces you want to highlight as key and either one string
+    composed of opening and closing strings of the same length,
+    such as \"[]\" or \"/**/\", to mark the beginning and end of
+    the region. Alternatively, you can also pass a list made up
+    of two strings, the opening and closing string, which then
+    don't need to be of the same size. See also
+    `turtles-mark-text-with-faces'"
+  (let ((calling-buf (current-buffer)))
+    (ert-with-test-buffer (:name name)
+      (turtles--internal-grab
+       frame win buf calling-buf minibuffer (mapcar #'car faces))
+      (when region
+        (turtles-mark-region (if (consp region) (car region) region)
+                              (if (consp region) (nth 1 region))))
+      (when point
+        (insert point))
+      (when faces (turtles-mark-text-with-faces faces))
+      (when trim
+        (delete-trailing-whitespace)
+        (while (eq ?\n (char-before (point-max)))
+          (delete-region (1- (point-max)) (point-max))))
+      (buffer-substring-no-properties (point-min) (point-max)))))
+
+(cl-defmacro turtles-with-grab-buffer ((&key (name "grab")
+                                              frame win buf minibuffer
+                                              faces)
+                                        &rest body)
+  "Grab a section of the terminal and store it into a test buffer.
+
+With no arguments, this function renders the current buffer in
+the turtles frame into an ERT test buffer and executes BODY.
+
+The ERT test buffer with the name \"grab\". When running
+interactively, ERT keeps such buffers when tests fail so they can
+be checked out. Specify the keyword argument NAME to modify the
+name of the test buffer.
+
+The garbbed buffer contains a textual representation of the frame
+or window captured on the turtles frame. When grabbing a buffer
+or window, the point and region will be grabbed as well.
+Additionally, unless FACES is specified, captured colors are
+available as overlay colors, within the limits of the turtles
+terminal, usually limited to 256 colors.
+
+More keyword arguments can be specified in parentheses, before
+BODY:
+
+  - The key argument BUF specifies a buffer to capture. It
+    defaults to the current buffer. The buffer is installed into
+    the single window of `turtles-frame', rendered, then
+    grabbed.
+
+  - The key argument WIN specifies a window to grab. The window
+    must be part of `turtles-frame'.
+
+  - Set the key argument MINIBUFFER to t to capture the content
+    of the minibuffer window of `turtles-frame'.
+
+  - Set the key argument FRAME to t to capture the whole frame.
+
+  - The key argument FACES asks for a specific set of faces to
+    be detected and grabbed. They'll be available as face
+    symbols set to the properties \\='face.
+
+    If necessary, faces can be made easier to test with text
+    comparison with `turtles-mark-text-with-faces'.
+
+    Note that colors won't be available in the grabbed buffer
+    content when FACES is specified."
+  (declare (indent 1))
+  (let ((calling-buf (make-symbol "calling-buf")))
+    `(let ((,calling-buf (current-buffer)))
+       (ert-with-test-buffer (:name ,name)
+         (turtles--internal-grab
+          ,frame ,win ,buf ,calling-buf ,minibuffer ,faces)
+
+         ,@body))))
+
+(defmacro turtles-read-from-minibuffer (read &rest body)
+  "Run BODY while executing READ.
+
+READ is a form that reads from the minibuffer and return the
+result.
+
+BODY is executed while READ is waiting for minibuffer input with
+the minibuffer active. Input can be provided by calling
+`execute-kbd-macro'. BODY must eventually either signal an error
+or exit the minibuffer.
+
+This macro allows mixing `execute-kbd-macro' and commands
+manipulating minibuffer with grab commands such as
+`turtles-to-string' and `turtles-with-grab-buffer'. `should'
+can also be called directly on BODY.
+
+This is provided here as a replacement to `ert-simulate-keys', as
+the approach taken by `ert-simulate-keys' doesn't allow grabbing
+intermediate states. This is because Emacs won't redisplay as
+long as there's pending input.
+
+Return whatever READ eventually evaluates to."
+  (declare (indent 1))
+  (let ((mb-result-var (make-symbol "mb-result")))
+    `(progn
+       (run-with-timer
+        0 nil
+        (lambda ()
+          (setq ,mb-result-var (progn ,read))))
+       (run-with-timer
+        0 nil
+        (lambda ()
+          (progn ,@body)
+          (when (active-minibuffer-window)
+            (error "Minibuffer still active at end of body form"))))
+       (sleep-for 0.01)
+       ,mb-result-var)))
+
+(defun turtles--internal-grab (frame win buf calling-buf minibuffer grab-faces)
+  "Internal macro implementation for grabbing into the current buffer.
+
+Do not call this function outside of this file."
+  (let ((cur (current-buffer)))
+  (cond
+   (buf (turtles-grab-buffer-into buf cur grab-faces))
+   (win (turtles-grab-window-into win cur grab-faces))
+   (minibuffer (turtles-grab-window-into (active-minibuffer-window) cur grab-faces))
+   (frame (turtles-grab-frame-into cur grab-faces))
+   (t (turtles-grab-buffer-into calling-buf cur grab-faces)))))
 
 (provide 'turtles-ert)
 
